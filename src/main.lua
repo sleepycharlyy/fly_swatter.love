fadf22262d31d7723aa407bb6b71c41388344e31
Charly <sleepycharlyy@hotmail.com>
2020-07-11 01:03:25 +0200

    added canvas and scaling to screen

--- src/main.lua
+++ src/main.lua
@@ -2,11 +2,18 @@ require("math");
 
 -- constants
 GAME_TITLE = "Fly Swatter";
-GAME_VERSION = 0.1
+GAME_VERSION = 0.1;
+WIDTH = 256;
+HEIGHT = 224;
 
 -- executes when game starts
 function love.load()
+    -- set title
     love.window.setTitle(GAME_TITLE.." "..GAME_VERSION);
+    -- set window mode
+    love.window.setMode(1280, 800);
+    -- create main canvas
+    canvas = love.graphics.newCanvas(WIDTH, HEIGHT);
 end
 
 -- called on every tick for calculations
@@ -16,7 +23,14 @@ end
 
 -- called on every tick for graphics
 function love.draw()
-    love.graphics.rectangle("fill", 200, 400, 200, 100);
+    -- render to canvas
+    canvas:renderTo(function()
+        love.graphics.setColor(1,1,1);
+        love.graphics.rectangle("fill", 0, 0, WIDTH, HEIGHT);
+    end)
+    
+    -- render canvas to screen and scale it to screen size
+    love.graphics.draw(canvas, 0, 0, 0, love.graphics.getWidth()/WIDTH, love.graphics.getHeight()/HEIGHT);
 end
 
 
