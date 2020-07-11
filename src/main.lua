3d94a975f5076599d66c809e6238528fc7ad4ea2
Charly <sleepycharlyy@hotmail.com>
2020-07-11 18:44:55 +0200

    idk sum changes

--- src/main.lua
+++ src/main.lua
@@ -6,6 +6,9 @@ GAME_VERSION = 0.1;
 WIDTH = 256;
 HEIGHT = 224;
 
+local spritesheet_tiles;
+local sprite_tile_1;
+
 -- executes when game starts
 function love.load()
     -- set title
@@ -19,8 +22,19 @@ function love.load()
         minheight = HEIGHT,
     });
 
+    -- set default scaling filter
+    love.graphics.setDefaultFilter('nearest', 'nearest');
+
     -- create main canvas
     canvas = love.graphics.newCanvas(WIDTH, HEIGHT);
+
+    -- create background canvas
+    background = love.graphics.newCanvas(WIDTH, HEIGHT);
+
+    -- create tiles spritebatch
+    spritesheet_tiles = love.graphics.newImage("assets/gfx/spritesheets/tiles.png");
+    sprite_tile_1 = love.graphics.newQuad(0, 0, 16, 16, spritesheet_tiles:getDimensions());
+
 end
 
 -- called on every tick for calculations
@@ -30,12 +44,28 @@ end
 
 -- called on every tick for graphics
 function love.draw()
-    -- render to canvas
-    canvas:renderTo(function()
-        love.graphics.setColor(1,1,1);
+    -- render background
+    background:renderTo(function ()
+        -- draw background color
+        love.graphics.setColor(18/255, 9/255, 25/255);
         love.graphics.rectangle("fill", 0, 0, WIDTH, HEIGHT);
-    end)
-    
+
+        -- draw tiles
+        for _x = 0, 15, 1 do
+            for _y = 0, 13, 1 do
+                if not((_x == 0) or (_y == 0) or (_x == 15) or (_y == 13)) then
+                    love.graphics.draw(spritesheet_tiles, _x*16, _y*16);
+                end
+            end
+        end
+    end);
+
+    -- render to main canvas
+    canvas:renderTo(function()
+        -- draw background
+        love.graphics.draw(background, 0, 0);
+    end);
+
     -- render canvas to screen and scale it to screen size
     love.graphics.draw(canvas, 0, 0, 0, love.graphics.getWidth()/WIDTH, love.graphics.getHeight()/HEIGHT);
 end
